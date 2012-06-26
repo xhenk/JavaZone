16dcd735621b5f12d535ccf52b7d243fb34a85be
unknown <xeb@PCXEB01.no.sterianet>
2012-06-26 15:46:20 +0200

    ny sysout

--- LeJOS/src/HelloWorld.java
+++ LeJOS/src/HelloWorld.java
@@ -4,6 +4,7 @@ import lejos.nxt.Motor;
 public class HelloWorld {
   public static void main (String[] args) {
     System.out.println("Hei.");
+    System.out.println("robot");
     Motor.A.forward();
     try {
 		Thread.sleep(2000L);
