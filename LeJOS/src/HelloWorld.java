efa429591d1a610727775f924461f5c7b1c1ad7f
unknown <xhenk@PCXHENK01.no.sterianet>
2012-06-26 15:21:40 +0200

    blablabla

--- LeJOS/src/HelloWorld.java
+++ LeJOS/src/HelloWorld.java
@@ -3,7 +3,7 @@ import lejos.nxt.Motor;
 
 public class HelloWorld {
   public static void main (String[] args) {
-    System.out.println("Hææææææææææææææææææææææææææææææææææææælo!");
+    System.out.println("Hei.");
     Motor.A.forward();
     try {
 		Thread.sleep(2000L);
