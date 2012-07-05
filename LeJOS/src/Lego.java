a83a47faf256d3eebf7e3fa18e90b154cbc86378
unknown <xhenk@PCXHENK01.no.sterianet>
2012-07-05 14:26:46 +0200

    Legosaker. Je.

--- LeJOS/src/Lego.java
+++ LeJOS/src/Lego.java
@@ -1,79 +1,52 @@
 import lejos.nxt.Button;
-import lejos.nxt.ColorSensor;
+import lejos.nxt.LCD;
 import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.SensorPortListener;
-import lejos.nxt.addon.ColorHTSensor;
+import lejos.nxt.TouchSensor;
+import lejos.nxt.UltrasonicSensor;
 
-public class HelloWorld {
-  public static void main (String[] args) {
-//	  SensorPort.S2.addSensorPortListener(new SensorPortListener() {
-//			
-//			@Override
-//			public void stateChanged(SensorPort aSource, int aOldValue, int aNewValue) {
-//				  Motor.B.rotate(20);
-//				
-//			}
-//		});
-    System.out.println("Hei.");
-    System.out.println("Nå får du godteri :)");
-//trekker opp     
-//    Motor.A.forward();
-//    try {
-//		Thread.sleep(2000L);
-//	} catch (InterruptedException e) {
-//		e.printStackTrace();
-//	}
-//    Motor.A.stop();
-    Motor.A.rotate(1000);
-    Motor.B.rotateTo(120);
-    Motor.A.rotate(-1000);
-    Motor.B.rotate(100);
-    Motor.B.rotateTo(0);
-    
-   
+public class Lego {
+	public static void main(String[] args) {
 
-    // 	Motor.A.waitComplete();
-   
-//    Motor.B.rotateTo(160);
-    
-    
-//   SensorPort.S1.addSensorPortListener(aListener);
-//   if{
-//	   ColorHTSensor.BLACK;
-//	   System.out.println("svart");
-//   }
-//   else{
-//	   System.out.println("Annen farge");
-//   }
-//   
-   
- //Fører over stoppe-pinne
-//    Motor.B.forward();
-//    try {
-//  		Thread.sleep(1000L);
-//  	} catch (InterruptedException e) {
-//  		e.printStackTrace();
-//  	}
-//    Motor.B.stop();
+		final long timestamp = System.currentTimeMillis();
+		final TouchSensor touch = new TouchSensor(SensorPort.S2);
+		final UltrasonicSensor uson = new UltrasonicSensor(SensorPort.S1);
+		new Thread() {
 
-// Løsner hyssingen
-//    Motor.A.backward();
-//    try {
-//  		Thread.sleep(2000L);
-//  	} catch (InterruptedException e) {
-//  		e.printStackTrace();
-//  	}
-//    Motor.A.stop();
-  
-// Tar tilbake stoppe-pinnen
-//    Motor.B.backward();
-//    try {
-//  		Thread.sleep(1000L);
-//  	} catch (InterruptedException e) {
-//  		e.printStackTrace();
-//  	}
-//    Motor.B.stop();
-  }
-  
+			public void run() {
+				for (;;) {
+					if (uson.getDistance() <= 10)
+						launch();
+					try {
+						sleep(100L);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+
+		}.start();
+		SensorPort.S2.addSensorPortListener(new SensorPortListener() {
+
+			@Override
+			public void stateChanged(SensorPort aSource, int aOldValue, int aNewValue) {
+				if (touch.isPressed())
+					launch();
+			}
+		});
+
+		Button.waitForAnyPress();
+
+	}
+
+	public static void launch() {
+
+		Motor.A.rotate(1000);
+		Motor.B.rotateTo(120);
+		Motor.A.rotate(-1000);
+		Motor.B.rotate(100);
+		Motor.B.rotateTo(0);
+	}
 }
